b146b719e4202d319d7dc1d700d05036766124a8
Ali Darwish <alidarwish502@gmail.com>
2023-05-18 15:52:41 +0300

    Installed react-icons library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import { FaCoffee } from 'react-icons/fa';
+
 function App() {
 
   return <>
     <button className="btn btn-primary">Click Here</button>
+    <FaCoffee className="mx-5 fs-1"/>
   </>;
 }
 
