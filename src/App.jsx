58518a9caf272626a5a232563b9631c763eb33ec
BOSS <ismael.swedan.fs@gmail.com>
2023-05-21 13:46:08 -0500

    Built the Home and Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-
-import { FaCoffee } from 'react-icons/fa';
-
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home"
 function App() {
 
   return <>
-    <button className="btn btn-primary">Click Here</button>
-    <FaCoffee className="mx-5 fs-1"/>
+    <Navbar />
+    <Home/>
   </>;
 }
 
