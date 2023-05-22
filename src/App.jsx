791de29a6fb49a0c132d4136e84366262aeac662
Ali Darwish <alidarwish502@gmail.com>
2023-05-22 14:38:51 +0300

    Built a NewPassword page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ForgetPassword from "./pages/ForgetPassword/FrogetPassword";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import NewPassword from "./pages/NewPassword/NewPassword";
 import Profile from "./pages/Profile/Profile";
 import Verify from "./pages/VerficationCode/Verify";
 
 // import Cart from "./pages/Cart/Cart";
 const user = {
-  name: 'Sara Ahmad',
-  birthDay: '15/8/1999',
-  pet : {
-    type : 'Cat'
-  }
-}
+  name: "Sara Ahmad",
+  birthDay: "15/8/1999",
+  pet: {
+    type: "Cat",
+  },
+};
 
 function App() {
-
-  return <>
-    {/* <Navbar />
+  return (
+    <>
+      {/* <Navbar />
     <Home/> */}
-    {/* <Profile user={user}/> */}
-    {/* <Login/>
+      {/* <Profile user={user}/> */}
+      {/* <Login/>
     <ForgetPassword/> */}
-    <Verify/>
-  </>;
+      {/* <Verify/> */}
+      <NewPassword />
+    </>
+  );
 }
 
 export default App;
