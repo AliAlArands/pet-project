50d20b9b2844e73e669cfc1bc278b70641b20e80
Ali Darwish <alidarwish502@gmail.com>
2023-05-24 14:41:44 +0300

    build the Navigation Menu.

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="tabs">
+        <span><a href="" className="navigation-link">Home</a></span>
+        <span><a href="" className="navigation-link">Find my pet</a></span>
+        <span><a href="" className="navigation-link">Support</a></span>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
