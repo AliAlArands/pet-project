e884d3ad2322636a5800034d9f80289ed7c63a68
Ali Darwish <alidarwish502@gmail.com>
2023-05-24 14:12:06 +0300

    Modified the Hero and Main Products sections in the home page.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,68 @@
-import './Home.css'
+import "./Home.css";
+import heroImage from "./../../assets/home/hero-img.jpg";
+import productImageOne from "./../../assets/home/pet-tracker-one.jpg";
+import productImageTwo from "./../../assets/home/pet-tracker-two.jpg";
 
-
-function Home () {
-return (
-        <section>
-        
-            <div className='top'>
-              <div className='left-side'>
-                <h1>taglinewrttaglintagli  <br /> ntaglcrintag</h1>
-                <p>At lacus vitae nulla sagittis scelerisque nisl <br />. Pellentesque duis cursus vestibulum, facilisi ac <br />, sed faucibus.</p>
-              </div>
-              <div className='image'>
-
-              </div>
-            </div>
-            <div className='toptwo'>
-              <h1>Main product description</h1>
-              <div className='productdescription' >
-                <div className='iimage'></div>
-                <p>Aenean egestas libero amet vulputate.</p>
-              </div>
-              <div className='productdescription' >
-                <p>Aenean egestas libero amet vulputate.</p>
-                <div className='iimage'></div>
-              </div>
-            </div>
-
-        </section>
-      )
-
-
+function Home() {
+  return (
+    <section>
+      <div className="hero">
+        <div className="intro">
+          <div className="left-side">
+            <h1>
+              Shop For Your <span className="pets-font-color">Pet's</span>{" "}
+              <br />
+              Happiness At <span className="pets-font-color">Pety</span>
+            </h1>
+            <p>
+              At lacus vitae nulla sagittis scelerisque nisl. Pellentesque
+              <br />
+              duis cursus vestibulum, facilisi ac, sed faucibus.
+            </p>
+            <button type="submit" className="btn btn-primary btn-shop">
+              Shop Now
+            </button>
+          </div>
+          <div>
+            <img
+              src={heroImage}
+              alt="dog and cat svg, pets svg"
+              className="img"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="main-product background-gc">
+        <h1 className="text-center main-product-desc">
+          Track your pet's adventures
+          <br /> with our GPS tracker
+        </h1>
+        <div className="product-description mb-5">
+          <div className="product-image">
+            <img
+              src={productImageOne}
+              alt="pet tracker product, pet store product, tracker, pet tracker"
+            />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ultricies.
+            Id ornare turpis vulputate enim sed magna sit. A id cursus{" "}
+          </p>
+        </div>
+        <div className="product-description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. ultricies.
+            Id ornare turpis vulputate enim sed magna sit. A id cursus{" "}
+          </p>
+          <div className="product-image">
+            <img
+              src={productImageTwo}
+              alt="pet tracker product, pet store product, tracker, pet tracker"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Home;
