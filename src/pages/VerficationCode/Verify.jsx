555413100708d9acd3a1eaf98a962a77dfe7e196
Ali Darwish <alidarwish502@gmail.com>
2023-05-25 22:06:19 +0300

    Modifed the Verify Page.

--- src/pages/VerficationCode/Verify.jsx
+++ src/pages/VerficationCode/Verify.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import "./verify.css";
 const Verify = () => {
   return (
-    <form className="form text-center" action="" method="POST">
-      <div className="h3 mb-3">Enter Verification Code</div>
-      <div className="mb-4">
-        <input type="text" maxlength="4" class="mb-3 input-field" />
-
-        <p>
-          If You Dont Recieve code 
-          <a href="#" className="link">
-            Resend
-          </a>
-        </p>
-      </div>
-      <button type="submit" className="btn btn-dark w-75 mb-5">
-        Send
-      </button>
-    </form>
+    <div className="background-gc contain">
+      <form className="form text-center" action="" method="POST">
+        <div className="title verify-title">Enter Verification Code</div>
+        <div className="mb-4">
+          <input type="text" maxlength="4" class="mb-3 input-field" />
+          <p>
+            If You Dont Recieve code{" "}
+            <a href="#" className="link">
+              Resend
+            </a>
+          </p>
+        </div>
+        <button type="submit" className="btn btn-primary w-75 mb-5 btn-send">
+          Send
+        </button>
+      </form>
+    </div>
   );
 };
 
