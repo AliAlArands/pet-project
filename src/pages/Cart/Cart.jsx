31ba8769cb44a100bac2250a6729edc4e777993d
Ali Darwish <alidarwish502@gmail.com>
2023-05-25 10:26:41 +0300

    Ahmad made feedback, find my pet and cart pages.

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="Title"> your card </h1>
+        <p className="text">
+          Lorem ipsum dolor sit amet consectetur <br />, adipisicing elit.
+          Dolorum deserunt suscipit
+        </p>
+        <button className="button">check out </button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/FindMyPet/FindMyPet.jsx
@@ -0,0 +1,18 @@
+import "./find.css";
+
+const FindMyPet = () => {
+  return (
+    <>
+      <div className="Findmybet">
+        <p className="text-center">Find my pet</p>
+
+        <input className="TEExt" type="text" placeholder="ID" />
+
+        {/* <input className='teext' type="text" /> <br /> */}
+        <button className="buttonn">tracker</button>
+      </div>
+    </>
+  );
+};
+
+export default FindMyPet;
