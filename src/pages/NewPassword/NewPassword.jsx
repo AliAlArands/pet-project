91433d10c9f166cbca6552edcb85de8e3d386cab
Ali Darwish <alidarwish502@gmail.com>
2023-05-25 21:52:35 +0300

    Modifed the NewPassword page.

--- src/pages/NewPassword/NewPassword.jsx
+++ src/pages/NewPassword/NewPassword.jsx
@@ -4,24 +4,24 @@ import "./newPassword.css";
 const NewPassword = () => {
   return (
     <>
-      <Form className="form">
-        <div className="h1 mb-4 text-center ">New Password</div>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label className="px-1">New Password</Form.Label>
-          <Form.Control type="password" />
-        </Form.Group>
-
-        <Form.Group className="mb-5" controlId="formBasicPassword">
-          <Form.Label className="px-1">Confirm Password</Form.Label>
-          <Form.Control type="password" />
-        </Form.Group>
-
-        <div className="d-flex justify-content-center flex-column align-items-center ">
-          <Button variant="primary" type="submit" className="w-100 mb-3">
-            Send
-          </Button>
-        </div>
-      </Form>
+      <div className="background-gc contain">
+        <Form className="form">
+          <div className="title">New Password</div>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+          <Form.Group className="mb-5" controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+          <div className="d-flex justify-content-center flex-column align-items-center ">
+            <Button variant="primary" type="submit" className="w-100 mb-3">
+              Send
+            </Button>
+          </div>
+        </Form>
+      </div>
     </>
   );
 };
