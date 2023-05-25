c8e1d140a3c0676a1ba1f9976fcac8a5478d0b5c
Ali Darwish <alidarwish502@gmail.com>
2023-05-25 10:30:13 +0300

    made changes to the css files so the font size become responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 import CardItem from "./components/Card/Card";
 import Navbar from "./components/Navbar/Navbar";
 import Checkout from "./pages/CheckOut/CheckOut";
 import Profile from "./pages/Profile/Profile";
 import SignUp from "./pages/SignUp/SignUp";
+import Home from "./pages/Home/Home";
+import Fqa from "./pages/FQA/Fqa";
+import Navigation from "./components/Navigation/Navigation";
 // import Cart from "./pages/Cart/Cart";
 const user = {
   name: "Sara Ahmad",
@@ -13,14 +17,22 @@ const user = {
   },
 };
 
+const product = {
+  title: "dry food",
+  stars: 4,
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aliquid cumque dolor ex, quis vero, repellendus pariatur a perspiciatis repudiandae, natus sunt! Aut, rem animi.",
+  price : "20$"  
+};
+
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Checkout/>
-      {/* <SignUp /> */}
-      {/* <CardItem /> */}
-      {/* <Profile user={user}/> */}
+      <Navbar />
+      {/* <Card product={product}/>
+      <Fqa/> */}
+      <Navigation/> 
+      <Home/>
     </div>
   );
 }
