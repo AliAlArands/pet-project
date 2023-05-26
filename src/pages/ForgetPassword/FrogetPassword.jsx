8ef691604b3b2e80343cb1c56b915ddc1ff27915
Ali Darwish <alidarwish502@gmail.com>
2023-05-26 10:53:13 +0300

    Modified the ForgetPassword Page.

--- src/pages/ForgetPassword/FrogetPassword.jsx
+++ src/pages/ForgetPassword/FrogetPassword.jsx
@@ -3,11 +3,11 @@ import Button from "react-bootstrap/Button";
 import "./forget.css";
 const ForgetPassword = () => {
   return (
-    <>
-      <Form className="form text-center w-25 px-5">
-        <div className="h3 mb-4">Forget Password</div>
-        <Form.Group className="mb-5 text-center w-100" controlId="formBasicEmail">
-          <Form.Label className="px-1 mb-1">Enter Your Email</Form.Label>
+    <div className="contain background-gc">
+      <Form className="form px-5">
+        <div className="title mb-4">Forget Password</div>
+        <Form.Group className="mb-5 w-100" controlId="formBasicEmail">
+          <Form.Label className=" mb-1">Enter Your Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
@@ -17,7 +17,7 @@ const ForgetPassword = () => {
           </Button>
         </div>
       </Form>
-    </>
+    </div>
   );
 };
 
