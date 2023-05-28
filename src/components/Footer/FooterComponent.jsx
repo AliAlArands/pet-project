76108c183c682b9abd9af8e336b45f1e4b4edb84
zeinab ali <rose.616.ali@gmail.com>
2023-05-28 14:25:30 -0400

    Modified footer component

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import './FooterComponent.css';
-import Logo from './../../assets/Logo.svg';
+import Logo from './../../assets/newlogo.svg';
+import { Link } from "react-router-dom";
 const FooterComponent = () => {
     return (
-        
+
         <footer>
             <div class="footer-contener">
                 <div class="left-side">
-                    <img src={Logo} alt="logo-image"/>
+                    <img src={Logo} alt="logo-image" />
                 </div>
                 <div class="center-side">
                     <p class="first-line">Aenean egestas libero amet vulputate.</p>
                     <p class="second-line">© 2022 Bonnie Hong. All Rights Reserved.</p>
                 </div>
+                <div className="links">
+                    <Link className='my-2' to='/home'>FAQ</Link>
+                    <Link className='my-2' to='/about'>Feedback</Link>
+                    <Link className='my-2' to='/about'>About us</Link>
+                </div>
                 <div class="right-side">
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-linkedin-in"></i>
+                <Link className='my-2' to='/https://www.instagram.com/'><i class="fa-brands fa-instagram"></i></Link>
+                <Link className='my-2' to='/https://www.linkedin.com/'> <i class="fa-brands fa-linkedin-in"></i></Link>
                 </div>
             </div>
         </footer>
