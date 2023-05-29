fc8c6325454ce9da9d16021e27f7d48003f83d23
Ali Darwish <alidarwish502@gmail.com>
2023-05-29 23:15:11 +0300

    Ismail modified the Navbar Component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,30 @@
 // import Navbar from "react-bootstrap/Navbar";
-import { FaBars, FaUser, FaShoppingCart, FaSearch } from "react-icons/fa";
 import "./NavbarStyle.css";
+import "bootstrap";
+import logo from "./../../assets/navbar/logo.svg";
+import user from "./../../assets/navbar/user.svg";
+import cart from "./../../assets/navbar/cart.svg";
+import { FiUser } from "react-icons/fi";
+import { BsCart2 } from "react-icons/bs";
+
 const Navbar = () => {
   return (
-    <div className="d-flex justify-content-between  align-items-center px-5 py-2 navbar-color">
-      <div className="d-flex justify-content-between align-items-center">
-        <FaBars className="me-3"/>
-        <div>LOGO</div>
+    <div className="d-flex justify-content-between align-items-center pet-store-navbar">
+      <div className="">
+        <img src={logo} alt="" className="logo" />
       </div>
 
-      <div className="d-flex justify-content-between align-items-center">
-        <form action="">
-          <div className="search-container">
-            <input type="text" className="input"/>
-            <FaSearch />
-          </div>
-        </form>
-        <FaShoppingCart className="me-4"/>
-        <FaUser />
+      <form action="" className="search-container">
+        <input type="text" className="search-nav-input" placeholder="Search for product"/>
+        <button>Search</button>
+      </form>
+
+      <div className="icons">
+        {/* <img src={cart} alt="" />
+        <img src={user} alt="" /> */}
+        <BsCart2/>
+        <FiUser />
+        
       </div>
     </div>
   );
