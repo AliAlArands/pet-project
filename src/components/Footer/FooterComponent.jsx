dcde55dd3bd2be2ece802971e9f15a1549b3263a
Ali Darwish <alidarwish502@gmail.com>
2023-05-30 12:50:45 +0300

    Modified the footer component.

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,24 +1,31 @@
 import React from "react";
-import './FooterComponent.css';
-import Logo from './../../assets/Logo.svg';
+import "./FooterComponent.css";
+import Logo from "./../../assets/navbar/footer-logo.svg";
+import { Link, NavLink } from "react-router-dom";
+import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
 const FooterComponent = () => {
-    return (
-        
-        <footer>
-            <div class="footer-contener">
-                <div class="left-side">
-                    <img src={Logo} alt="logo-image"/>
-                </div>
-                <div class="center-side">
-                    <p class="first-line">Aenean egestas libero amet vulputate.</p>
-                    <p class="second-line">© 2022 Bonnie Hong. All Rights Reserved.</p>
-                </div>
-                <div class="right-side">
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-linkedin-in"></i>
-                </div>
-            </div>
-        </footer>
-    );
-}
-export default Footer;
+  return (
+    <footer className="footer-container">
+      <img src={Logo} alt="logo" className="logo" />
+
+      <div class="center-side">
+        <p class="first-line">Aenean egestas libero amet vulputate.</p>
+        <p class="second-line">© 2022 Bonnie Hong. All Rights Reserved.</p>
+      </div>
+      <div className="footer-pages-links">
+        <NavLink to="/home">FAQ</NavLink>
+        <NavLink to="/feedback">Feedback</NavLink>
+        <NavLink to="/about">About us</NavLink>
+      </div>
+      <div class="right-side">
+        <Link className="footer-icons" to="#">
+          <FaInstagram />
+        </Link>
+        <Link className="footer-icons" to="#">
+          <FaLinkedinIn />
+        </Link>
+      </div>
+    </footer>
+  );
+};
+export default FooterComponent;
