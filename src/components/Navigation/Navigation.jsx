31d11aa9bc87d262acc8ca3571cd49ba19dd5d25
Ali Darwish <alidarwish502@gmail.com>
2023-05-30 11:36:28 +0300

    Modified the Navigation component for the last time.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./navigation.css";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div className="navigation">
       <div className="tabs">
-        <span><a href="" className="navigation-link">Home</a></span>
-        <span><a href="" className="navigation-link">Find my pet</a></span>
-        <span><a href="" className="navigation-link">Support</a></span>
+        <span>
+          <NavLink to="/" className="navigation-link">
+            Home
+          </NavLink>
+        </span>
+        <span>
+          <NavLink to="/findmypet" activeClassName="active" className="navigation-link">
+            Find my pet
+          </NavLink>
+        </span>
+        <span>
+          <NavLink to="/support" activeClassName="active" className="navigation-link">
+            Support
+          </NavLink>
+        </span>
       </div>
     </div>
   );
