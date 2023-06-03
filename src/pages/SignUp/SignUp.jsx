9daa53dca99c5a35d802d34b4636f6721a9b3340
ali <alidarwish502@gmail.com>
2023-06-03 20:21:45 +0200

    fixed a typo.

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -24,7 +24,7 @@ const SignUp = () => {
       confirmPassword: confirmPasswordRef
     };
 
-    petStore.post("/regiter/", formData)
+    petStore.post("/register/", formData)
       .then(response => {
         console.log("log in response data:", response.data);
       })
